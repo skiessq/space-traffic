1b885b2cfc750b1762a26e096653473252c37824
skiessq <skiessq@gmail.com>
2026-08-19 19:10:00 +0100

    Initial commit without large datasets or local database

---
+++ src/database.py
@@ -0,0 +1,134 @@
+import duckdb
+
+def init_db():
+    with duckdb.connect("space_traffic.db") as con:
+        con.sql("CREATE TABLE IF NOT EXISTS celestrak (" \
+            "OBJECT_ID VARCHAR(50), " \
+            "OBJECT_NAME VARCHAR(255), " \
+            "EPOCH TIMESTAMP, " \
+            "MEAN_MOTION DOUBLE, " \
+            "ECCENTRICITY DOUBLE, " \
+            "INCLINATION DOUBLE, " \
+            "RA_OF_ASC_NODE DOUBLE, " \
+            "ARG_OF_PERICENTER DOUBLE, " \
+            "MEAN_ANOMALY DOUBLE, " \
+            "EPHEMERIS_TYPE INTEGER, " \
+            "CLASSIFICATION_TYPE VARCHAR(10), " \
+            "NORAD_CAT_ID INTEGER PRIMARY KEY, " \
+            "ELEMENT_SET_NO INTEGER, " \
+            "REV_AT_EPOCH INTEGER, " \
+            "BSTAR DOUBLE, " \
+            "MEAN_MOTION_DOT DOUBLE, " \
+            "MEAN_MOTION_DDOT DOUBLE" \
+        ")")
+        con.sql("CREATE TABLE IF NOT EXISTS space_conjunctions (" \
+                "split VARCHAR NOT NULL, " \
+                "event_id BIGINT NOT NULL, " \
+                "time_to_tca DOUBLE NOT NULL, " \
+                "mission_id BIGINT, " \
+                "risk DOUBLE, " \
+                "max_risk_estimate DOUBLE, " \
+                "max_risk_scaling DOUBLE, " \
+                "miss_distance DOUBLE, " \
+                "relative_speed DOUBLE, " \
+                "relative_position_r DOUBLE, " \
+                "relative_position_t DOUBLE, " \
+                "relative_position_n DOUBLE, " \
+                "relative_velocity_r DOUBLE, " \
+                "relative_velocity_t DOUBLE, " \
+                "relative_velocity_n DOUBLE, " \
+                "t_time_lastob_start DOUBLE, " \
+                "t_time_lastob_end DOUBLE, " \
+                "t_recommended_od_span DOUBLE, " \
+                "t_actual_od_span DOUBLE, " \
+                "t_obs_available BIGINT, " \
+                "t_obs_used BIGINT, " \
+                "t_residuals_accepted DOUBLE, " \
+                "t_weighted_rms DOUBLE, " \
+                "t_rcs_estimate DOUBLE, " \
+                "t_cd_area_over_mass DOUBLE, " \
+                "t_cr_area_over_mass DOUBLE, " \
+                "t_sedr DOUBLE, " \
+                "t_j2k_sma DOUBLE, " \
+                "t_j2k_ecc DOUBLE, " \
+                "t_j2k_inc DOUBLE, " \
+                "t_ct_r DOUBLE, " \
+                "t_cn_r DOUBLE, " \
+                "t_cn_t DOUBLE, " \
+                "t_crdot_r DOUBLE, " \
+                "t_crdot_t DOUBLE, " \
+                "t_crdot_n DOUBLE, " \
+                "t_ctdot_r DOUBLE, " \
+                "t_ctdot_t DOUBLE, " \
+                "t_ctdot_n DOUBLE, " \
+                "t_ctdot_rdot DOUBLE, " \
+                "t_cndot_r DOUBLE, " \
+                "t_cndot_t DOUBLE, " \
+                "t_cndot_n DOUBLE, " \
+                "t_cndot_rdot DOUBLE, " \
+                "t_cndot_tdot DOUBLE, " \
+                "c_object_type VARCHAR, " \
+                "c_time_lastob_start DOUBLE, " \
+                "c_time_lastob_end DOUBLE, " \
+                "c_recommended_od_span DOUBLE, " \
+                "c_actual_od_span DOUBLE, " \
+                "c_obs_available DOUBLE, " \
+                "c_obs_used DOUBLE, " \
+                "c_residuals_accepted DOUBLE, " \
+                "c_weighted_rms DOUBLE, " \
+                "c_rcs_estimate DOUBLE, " \
+                "c_cd_area_over_mass DOUBLE, " \
+                "c_cr_area_over_mass DOUBLE, " \
+                "c_sedr DOUBLE, " \
+                "c_j2k_sma DOUBLE, " \
+                "c_j2k_ecc DOUBLE, " \
+                "c_j2k_inc DOUBLE, " \
+                "c_ct_r DOUBLE, " \
+                "c_cn_r DOUBLE, " \
+                "c_cn_t DOUBLE, " \
+                "c_crdot_r DOUBLE, " \
+                "c_crdot_t DOUBLE, " \
+                "c_crdot_n DOUBLE, " \
+                "c_ctdot_r DOUBLE, " \
+                "c_ctdot_t DOUBLE, " \
+                "c_ctdot_n DOUBLE, " \
+                "c_ctdot_rdot DOUBLE, " \
+                "c_cndot_r DOUBLE, " \
+                "c_cndot_t DOUBLE, " \
+                "c_cndot_n DOUBLE, " \
+                "c_cndot_rdot DOUBLE, " \
+                "c_cndot_tdot DOUBLE, " \
+                "t_span DOUBLE, " \
+                "c_span DOUBLE, " \
+                "t_h_apo DOUBLE, " \
+                "t_h_per DOUBLE, " \
+                "c_h_apo DOUBLE, " \
+                "c_h_per DOUBLE, " \
+                "geocentric_latitude DOUBLE, " \
+                "azimuth DOUBLE, " \
+                "elevation DOUBLE, " \
+                "mahalanobis_distance DOUBLE, " \
+                "t_position_covariance_det DOUBLE, " \
+                "c_position_covariance_det DOUBLE, " \
+                "t_sigma_r DOUBLE, " \
+                "c_sigma_r DOUBLE, " \
+                "t_sigma_t DOUBLE, " \
+                "c_sigma_t DOUBLE, " \
+                "t_sigma_n DOUBLE, " \
+                "c_sigma_n DOUBLE, " \
+                "t_sigma_rdot DOUBLE, " \
+                "c_sigma_rdot DOUBLE, " \
+                "t_sigma_tdot DOUBLE, " \
+                "c_sigma_tdot DOUBLE, " \
+                "t_sigma_ndot DOUBLE, " \
+                "c_sigma_ndot DOUBLE, " \
+                "F10 DOUBLE, " \
+                "F3M DOUBLE, " \
+                "SSN DOUBLE, " \
+                "AP DOUBLE, " \
+                "PRIMARY KEY (event_id, time_to_tca, split)"
+        ")")
+
+if __name__ == "__main__":
+    init_db()
+
---
+++ src/ingestion.py
@@ -0,0 +1,67 @@
+import csv
+import duckdb
+import requests
+import json
+import os
+
+def check_celestrak_cache_folder():
+    dir_name = './dataset/celestrak_cache'
+    if os.path.isdir(dir_name):
+        if not os.listdir(dir_name):
+            print("Directory is empty. Downloading JSON Data.")
+            return True
+        else:    
+            print("Directory is not empty. JSON Data cached")
+            return False
+    else:
+        os.mkdir(dir_name)
+        print("Directory created. Downloading JSON Data.")
+        return True
+ 
+def download_celestrak_data(group="ACTIVE"):
+    output_file = "./dataset/celestrak_cache/celestrak_data.json"
+    url = f"https://celestrak.org/NORAD/elements/gp.php?GROUP={group}&FORMAT=JSON"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    }
+    request = requests.get(url, headers=headers)
+    if request.status_code == 200:
+        with open(output_file, 'w') as f:
+            json.dump(request.json(), f)
+        print("Downloaded JSON data from Celestrak and saved to celestrak_data.json")
+        return True
+
+    print("Failed to download JSON data from Celestrak. Status code:", request.status_code)
+    return False
+
+def ingest_api_data(con):
+    if check_celestrak_cache_folder():
+        if download_celestrak_data():
+            cache_file = './dataset/celestrak_cache/celestrak_data.json'
+            con.sql(f"INSERT INTO celestrak SELECT * FROM read_json_auto('{cache_file}') ON CONFLICT (NORAD_CAT_ID) DO NOTHING")
+            print("Ingested Celestrak data into the database.")
+
+def ingest_csv_data(con):
+    if not os.path.exists('./dataset/train_data.csv'):
+            print("Train data CSV file not found. Please ensure the file exists at './dataset/train_data.csv'.")
+            print("Links are provided in dataset/dataset.txt to download the train data.")
+            return
+    
+    if not os.path.exists('./dataset/test_data.csv'):
+        print("Test data CSV file not found. Please ensure the file exists at './dataset/test_data.csv'.")
+        print("Links are provided in dataset/dataset.txt to download the test data.")
+        return
+
+    con.sql("INSERT INTO space_conjunctions SELECT 'train' AS split,* FROM read_csv_auto('./dataset/train_data.csv') ON CONFLICT (event_id, time_to_tca, split) DO NOTHING")
+    print("Ingested train data into the database.")
+
+    con.sql("INSERT INTO space_conjunctions SELECT 'test' AS split,* FROM read_csv_auto('./dataset/test_data.csv') ON CONFLICT (event_id, time_to_tca, split) DO NOTHING")
+    print("Ingested test data into the database.")
+
+def run_ingestion():
+  with duckdb.connect("space_traffic.db") as con:
+    ingest_csv_data(con)
+    ingest_api_data(con)
+
+if __name__ == "__main__":
+    run_ingestion()
